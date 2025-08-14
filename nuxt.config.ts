ea9087f5a9bb30b1d3f6d41fb452bb2eabf16d58
anakais-cactus <alexandre.nakai@cactusgaming.net>
2025-08-14 14:31:23 +0300

    fix: meta warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
         },
       ],
       meta: [
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
         {
           name: 'apple-mobile-web-app-status-bar-style',
           content: 'black-translucent',
